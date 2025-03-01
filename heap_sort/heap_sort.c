332ca398a4e2d3185b3956e9d640809d854c0b8e
garou404 <charlesmeyer347@gmail.com>
2025-03-01 18:04:56 +0100

    starting heap sort implementation and README added

---
+++ heap_sort/heap_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* heap_sort(int*tab, int size){
+    int* heap = build_the_heap(tab, size);
+}
+
+int* build_the_heap(int*tab, int size){
+    int* heap = malloc(sizeof(int)*size);
+    
+}
+
+int main(int argc, char** argv) {
+    return EXIT_SUCCESS;
+}
